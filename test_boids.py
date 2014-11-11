00bee56b8f5bfcf1ac4d73170e6309a83d5aca1a
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-11 16:57:20 +0000

    Changed test_boids.py but the regression test countinues to fail.

--- test_boids.py
+++ test_boids.py
@@ -7,13 +7,11 @@ from class_boid import boid
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
     data_before=regression_data["before"]
-    boids_before=[boid(data_before[i]['x_position'],data_before[i]['y_position'],data_before[i]['x_velocity'],data_before[i]['y_velocity']) for i in range(50)]
+    boids_before=[boid(data_before[0][i],data_before[1][i],data_before[2][i],data_before[3][i]) for i in range(50)]
     update_boids(boids_before)
     
     data_after=regression_data["after"]
-    boids_after=[boid(data_after[i]['x_position'],data_after[i]['y_position'],data_after[i]['x_velocity'],data_after[i]['y_velocity']) for i in range(50)]
+    boids_after=[boid(data_after[0][i],data_after[1][i],data_after[2][i],data_after[3][i]) for i in range(50)]
 
-    for after,before in zip(boids_after,boids_before):
-        assert_almost_equal(after.x_position,before.x_position,delta=0.01)
-	
-test_bad_boids_regression()
+    for a,b in zip(boids_after,boids_before):
+        assert_almost_equal(a.x_position,b.x_position,delta=0.01)
