d6069f9cef46c3e3aab8f09f27113af3e9f68e96
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-11 00:47:04 +0000

    Created method fly_towards_middle in the boid class which will replace the one in boids.py

--- class_boid.py
+++ class_boid.py
@@ -5,3 +5,9 @@ class boid(object):
        self.x_velocity=x_velocity
        self.y_velocity=y_velocity
 
+   def fly_towards_middle(self, other_boid, weight):
+      self.x_velocity += (other_boid.x_position-self.x_position)*weight
+      self.y_velocity += (other_boid.y_position-self.y_position)*weight
+     
+
+
