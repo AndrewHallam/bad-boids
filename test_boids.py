247939e19528c829a4c5aff83148332f5fac2148
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-11 22:40:05 +0000

    Slightly changed test_fly_towards_middle() so it actually works properly with two boids.

--- test_boids.py
+++ test_boids.py
@@ -23,9 +23,10 @@ def test_fly_towards_middle():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fly_towards_middle.yml')))
     data_before=regression_data["before"]
     boids_before=[boid(data_before[0][i],data_before[1][i],data_before[2][i],data_before[3][i]) for i in range(2)]
-    boids_before[0].fly_towards_middle(boids_before[1], 1, 1)
-    print '{},{},{},{}'.format(boids_before[0].x_position, boids_before[0].y_position, boids_before[0].x_velocity, boids_before[0].y_velocity)
-    boids_after=[boid(0, 0, 1, 1), boid(1, 1, 0, 0)]
+
+    boids_before[0].fly_towards_middle(boids_before[1], 1., 2.)
+
+    boids_after=[boid(0, 0, 0.5, 0.5), boid(1, 1, 0, 0)]
     
     for after,before in zip(boids_after,boids_before):
         assert_almost_equal(after.x_position,before.x_position,delta=0.01)
@@ -33,4 +34,4 @@ def test_fly_towards_middle():
         assert_almost_equal(after.x_velocity,before.x_velocity,delta=0.01)
         assert_almost_equal(after.y_velocity,before.y_velocity,delta=0.01)
 
-def 
+test_fly_towards_middle()
