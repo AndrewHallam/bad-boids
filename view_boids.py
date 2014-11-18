8c3b5acb37cbb60e2cf45fe7664930a4d1f4e8c6
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-18 23:59:30 +0000

    I changed view_boids.py to deal with the new boidsbuilderrandom class

--- view_boids.py
+++ view_boids.py
@@ -1,26 +1,20 @@
-from boids import Boids, BoidsBuilder
+from boids import BoidsModel, BoidsBuilderRandom
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-builder=BoidsBuilder()
+builder=BoidsBuilderRandom()
 builder.start_boids()
 builder.set_starling_properties(0.01/50, 10, 100, 0.125/50)
 builder.set_eagle_properties(100, 5000, 0.00005)
-boids=builder.finish()
-#boids=Boids(
-#        flock_attraction=0.01/50,
-#        avoidance_radius=10,
-#        formation_flying_radius=100,
-#        speed_matching_strength=0.125/50
-#    )
+builder.initialise(50)
+builder.add_eagle(0,0,0,50)
+boidsmodel=builder.finish()
 
-
-boids.initialise_random(50)
-boids.add_eagle(0,0,0,50)
-
+#
 figure=plt.figure()
 axes=plt.axes(xlim=(-2000,1500), ylim=(-500,4000))
-scatter=axes.scatter([b.position[0] for b in boids.boids],[b.position[1] for b in boids.boids])
+scatter=axes.scatter([b.position[0] for b in boidsmodel.boids],[b.position[1] for b in boidsmodel.boids])
+
 
 def color(boid):
 	if type(boid)=="Eagle":
@@ -28,9 +22,10 @@ def color(boid):
 	return (0,0,1)
 
 def animate(frame):
-    boids.update()
-    scatter.set_offsets([b.position for b in boids.boids])
-    scatter.set_color([b.colour() for b in boids.boids])
+    boidsmodel.update()
+    scatter.set_offsets([b.position for b in boidsmodel.boids])
+    scatter.set_color([b.colour() for b in boidsmodel.boids])
+
 
 
 anim = animation.FuncAnimation(figure, animate,
@@ -38,3 +33,28 @@ anim = animation.FuncAnimation(figure, animate,
 
 if __name__ == "__main__":
     plt.show()
+
+#from boids import Boids, BoidsBuilderRandom
+#from matplotlib import pyplot as plt
+#from matplotlib import animation
+#
+#builder=BoidsBuilderRandom()
+#builder.start_boids()
+#builder.set_starling_properties(0.01/50, 10, 100, 0.125/50)
+#builder.set_eagle_properties(100, 5000, 0.00005)
+#builder.initialise_random(50)
+#builder.add_eagle(0,0,0,50)
+#boids=builder.finish()
+
+#from boids import Boids, BoidsBuilder
+#from matplotlib import pyplot as plt
+#from matplotlib import animation
+#
+#builder=BoidsBuilder()
+#builder.start_boids()
+#builder.set_starling_properties(0.01/50, 10, 100, 0.125/50)
+#builder.set_eagle_properties(100, 5000, 0.00005)
+#boids=builder.finish()
+#boids.initialise_random(50)
+#boids.add_eagle(0,0,0,50)
+#
